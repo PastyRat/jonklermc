df770cbfcef0aa0dfb9b7432b0163a915e55574a
Angus Ollier <angus.ollier@outlook.com>
2024-09-02 19:25:39 +0100

    Actually fixed Jonkler item drop and made the drop a random amount

--- src/main/java/net/mcreator/thejonkler/entity/JonklermonsterEntity.java
+++ src/main/java/net/mcreator/thejonkler/entity/JonklermonsterEntity.java
@@ -33,6 +33,7 @@ import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.chat.Component;
 
+import net.mcreator.thejonkler.procedures.JonklermonsterEntityDiesProcedure;
 import net.mcreator.thejonkler.init.TheJonklerModItems;
 import net.mcreator.thejonkler.init.TheJonklerModEntities;
 
@@ -116,6 +117,12 @@ public class JonklermonsterEntity extends Monster {
 	@Override
 	public boolean fireImmune() {
 		return true;
+	}
+
+	@Override
+	public void die(DamageSource source) {
+		super.die(source);
+		JonklermonsterEntityDiesProcedure.execute(this.level(), this.getX(), this.getY(), this.getZ());
 	}
 
 	@Override
